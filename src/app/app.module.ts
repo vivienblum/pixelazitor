b3f437755199d1c0732dc89369e55dc34177261e
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-07 10:37:52 +0100

    :zap: Upload and preview image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { UploadComponent } from './upload/upload.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 const appRoutes: Routes = [
-  { path: "", component: HomeComponent }
+  { path: "", component: HomeComponent },
+  { path: "upload", component: UploadComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UploadComponent
   ],
   imports: [
     RouterModule.forRoot(
@@ -22,7 +27,8 @@ const appRoutes: Routes = [
       { enableTracing: true }
     ),
     BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
