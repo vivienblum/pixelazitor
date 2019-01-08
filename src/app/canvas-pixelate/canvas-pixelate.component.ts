1131e2133f3f4a3209d8d079b5978ef26217701f
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-08 14:49:12 +0100

    :lipstick: Refactoring

--- src/app/canvas-pixelate/canvas-pixelate.component.ts
+++ src/app/canvas-pixelate/canvas-pixelate.component.ts
@@ -21,46 +21,31 @@ export class CanvasPixelateComponent implements OnInit {
   set amount(amount: number) {
     if (amount && this._amount !== amount) {
       this._amount = amount
-      console.log("Setting ...")
       if (this.cx) {
-        console.log("Setting image ... ", this._amount)
         this.pixelateImage(this._image, this._amount)
       }
-      //
     }
   }
 
   private cx: CanvasRenderingContext2D
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement
+    // image
     this._image = new Image()
     this._image.src =
       "https://www.html5canvastutorials.com/demos/assets/darth-vader.jpg"
-  }
-
-  ngOnInit() {
-    console.log("Init")
-    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement
     // canvas
     this.cx = canvasEl.getContext("2d")
-    // this.cx = this.disableSmoothRendering(this.cx)
+
     this._image.onload = function() {
       canvasEl.width = this._image.width
       canvasEl.height = this._image.height
       this.cx.drawImage(this._image, 0, 0)
-      // this.cx = this.disableSmoothRendering(this.cx)
     }.bind(this)
-  }
-
-  ngAfterViewInit() {
-    console.log("After view init")
-    // this._image.onload = function() {
-    //   canvasEl.width = this._image.width
-    //   canvasEl.height = this._image.height
-    //   this.cx.drawImage(this._image, 0, 0)
-    //   // this.cx = this.disableSmoothRendering(this.cx)
-    // }.bind(this)
-    // Test
-    // this.image.src = this.canvas.toDataURL('image/png');
   }
 
   disableSmoothRendering(cx) {
@@ -71,7 +56,7 @@ export class CanvasPixelateComponent implements OnInit {
     return cx
   }
 
-  pixelateImage(imageLoc, amount) {
+  pixelateImage(image, amount) {
     const canvasEl: HTMLCanvasElement = this.canvas.nativeElement
 
     this.cx = this.disableSmoothRendering(this.cx)
@@ -79,20 +64,10 @@ export class CanvasPixelateComponent implements OnInit {
     var w = canvasEl.width * (amount <= 0 ? 0.01 : amount)
     var h = canvasEl.height * (amount <= 0 ? 0.01 : amount)
 
-    // render smaller imageLoc
-    this.cx.drawImage(imageLoc, 0, 0, w, h)
-    // stretch the smaller imageLoc
-    this.cx.drawImage(
-      canvasEl,
-      0,
-      0,
-      w,
-      h,
-      0,
-      0,
-      imageLoc.width,
-      imageLoc.height
-    )
+    // render smaller image
+    this.cx.drawImage(image, 0, 0, w, h)
+    // stretch the smaller image
+    this.cx.drawImage(canvasEl, 0, 0, w, h, 0, 0, image.width, image.height)
   }
 
   get amount(): number {
