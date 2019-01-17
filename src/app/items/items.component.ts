777836e820b535351fee01f28e99443b49d06dc2
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-17 11:17:04 +0100

    :construction: Added data

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
+
+const items = [
+  {
+    id: 1,
+    name: "Leffe",
+    collection: 5,
+    blue: 79,
+    green: 84,
+    red: 89
+  }
+]
 
 @Component({
-  selector: 'app-items',
-  templateUrl: './items.component.html',
-  styleUrls: ['./items.component.scss']
+  selector: "app-items",
+  templateUrl: "./items.component.html",
+  styleUrls: ["./items.component.scss"]
 })
 export class ItemsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
