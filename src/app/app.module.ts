6058fffe76f099b76db54d32cb4c60dd3169cb87
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-17 15:50:21 +0100

    :sparkles: Toggle works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatSliderModule } from "@angular/material/slider"
 import { MatStepperModule } from "@angular/material/stepper"
 import { MatListModule } from "@angular/material/list"
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner"
+import { MatButtonToggleModule } from "@angular/material/button-toggle"
 
 import { AppComponent } from "./app.component"
 import { HomeComponent } from "./home/home.component"
@@ -20,10 +21,10 @@ import { PixelateComponent } from "./pixelate/pixelate.component"
 import { CanvasPixelateComponent } from "./canvas-pixelate/canvas-pixelate.component"
 import { StepperComponent } from "./stepper/stepper.component"
 import { MatchComponent } from "./match/match.component"
-import { ItemsComponent } from "./items/items.component";
-import { PatternComponent } from './pattern/pattern.component';
-import { CanvasPatternComponent } from './canvas-pattern/canvas-pattern.component';
-import { CanvasPatternImagesComponent } from './canvas-pattern-images/canvas-pattern-images.component'
+import { ItemsComponent } from "./items/items.component"
+import { PatternComponent } from "./pattern/pattern.component"
+import { CanvasPatternComponent } from "./canvas-pattern/canvas-pattern.component"
+import { CanvasPatternImagesComponent } from "./canvas-pattern-images/canvas-pattern-images.component"
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
@@ -59,6 +60,7 @@ const appRoutes: Routes = [
     MatSliderModule,
     MatStepperModule,
     MatProgressSpinnerModule,
+    MatButtonToggleModule,
     MatListModule
   ],
   providers: [],
--- src/app/pattern/pattern.component.ts
+++ src/app/pattern/pattern.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core"
 import { Observable } from "rxjs"
+import { MatButtonToggleModule } from "@angular/material/button-toggle"
 import { Item } from "../models/item"
 
 @Component({
