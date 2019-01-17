4358a109efccb3f43d45fe89bc202a421d4518d6
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-17 11:08:19 +0100

    :recycle: Refactoring with basUrl from environement

--- src/app/services/match.service.ts
+++ src/app/services/match.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from "@angular/core"
 import { HttpClient } from "@angular/common/http"
 import { Observable } from "rxjs"
+import { environment } from "../environments/environment"
 
 @Injectable({
   providedIn: "root"
 })
 export class MatchService {
+  baseUrl = environment.baseUrl
+
   constructor(private http: HttpClient) {}
 
   public add(fd: FormData): Observable<any> {
-    const url = "https://match-images.herokuapp.com/api/"
-
-    return this.http.post<any>(`${url}matches/`, fd)
+    return this.http.post<any>(`${this.baseUrl}matches/`, fd)
   }
 }
