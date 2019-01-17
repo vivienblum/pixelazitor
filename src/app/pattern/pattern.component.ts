9b48a8d7c7d6e122b6bfa99f4b437b995626adf8
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-17 13:57:23 +0100

    :sparkles: Passing items through match works

--- src/app/pattern/pattern.component.ts
+++ src/app/pattern/pattern.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core"
+import { Observable } from "rxjs"
+import { Item } from "../models/item"
 
 @Component({
-  selector: 'app-pattern',
-  templateUrl: './pattern.component.html',
-  styleUrls: ['./pattern.component.scss']
+  selector: "app-pattern",
+  templateUrl: "./pattern.component.html",
+  styleUrls: ["./pattern.component.scss"]
 })
 export class PatternComponent implements OnInit {
+  private _items: Observable<Item[]>
 
-  constructor() { }
-
-  ngOnInit() {
+  @Input()
+  set items(items: Observable<Item[]>) {
+    this._items = items
   }
 
+  constructor() {}
+
+  ngOnInit() {}
+
+  get items(): Observable<Item[]> {
+    return this._items
+  }
 }
