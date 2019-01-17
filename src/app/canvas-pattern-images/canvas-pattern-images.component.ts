626c7abc16d25609ae2238bfa068e605059e5838
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-17 15:07:26 +0100

    :sparkles: Display pattern images table works

--- src/app/canvas-pattern-images/canvas-pattern-images.component.ts
+++ src/app/canvas-pattern-images/canvas-pattern-images.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core"
 import { Observable } from "rxjs"
 import { Item } from "../models/item"
+import { environment } from "../../environments/environment"
 
 @Component({
   selector: "app-canvas-pattern-images",
@@ -10,6 +11,7 @@ import { Item } from "../models/item"
 export class CanvasPatternImagesComponent implements OnInit {
   private _items: Observable<Item[]>
   private _pattern: number[][]
+  baseUrl = environment.baseUrl
 
   @Input()
   set items(items: Observable<Item[]>) {
@@ -19,7 +21,6 @@ export class CanvasPatternImagesComponent implements OnInit {
   @Input()
   set pattern(pattern: number[][]) {
     this._pattern = pattern
-    console.table(this._pattern)
   }
 
   constructor() {}
@@ -32,5 +33,26 @@ export class CanvasPatternImagesComponent implements OnInit {
 
   get pattern(): number[][] {
     return this._pattern
+  }
+
+  get patternImages(): string[][] {
+    let itemsIndexed: number[] = new Array(this._items.length)
+    this._items.forEach(item => {
+      itemsIndexed[item.id] = item
+    })
+
+    let patternImages: string[][] = new Array(this._pattern.length)
+    this._pattern.forEach((row, y) => {
+      patternImages[y] = new Array(row.length)
+      row.forEach((el, x) => {
+        this._items.filter(item => {
+          return (item.id = el)
+        })
+
+        patternImages[y][x] = el >= 0 ? itemsIndexed[el] : null
+      })
+    })
+    console.table(patternImages)
+    return patternImages
   }
 }
