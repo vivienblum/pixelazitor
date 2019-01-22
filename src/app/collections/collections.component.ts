5c9b5affe869d4daad83f23ece59228a0dc0cd01
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-22 10:57:15 +0100

    :sparkles: Display all collections

--- src/app/collections/collections.component.ts
+++ src/app/collections/collections.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
+import { Collection } from "../models/collection"
+import { CollectionService } from "../services/collection.service"
+import { Observable } from "rxjs"
+import { MatListModule } from "@angular/material/list"
 
 @Component({
-  selector: 'app-collections',
-  templateUrl: './collections.component.html',
-  styleUrls: ['./collections.component.scss']
+  selector: "app-collections",
+  templateUrl: "./collections.component.html",
+  styleUrls: ["./collections.component.scss"]
 })
 export class CollectionsComponent implements OnInit {
+  private _collections: Observable<Collection[]>
 
-  constructor() { }
+  constructor(private collectionService: CollectionService) {}
 
   ngOnInit() {
+    this._collections = this.collectionService.getCollections()
   }
 
+  get collections(): Observable<Collection[]> {
+    return this._collections
+  }
 }
