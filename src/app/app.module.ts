e4e74f04f315c8130193f65ea52e15a68e6ffdda
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-24 16:54:12 +0100

    :children_crossing: Added footbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,7 +37,8 @@ import { CollectionsComponent } from "./collections/collections.component"
 import { CollectionCreationComponent } from "./collection-creation/collection-creation.component"
 import { CollectionItemsComponent } from "./collection-items/collection-items.component"
 import { ItemCreationComponent } from "./item-creation/item-creation.component"
-import { ColorBadgeComponent } from "./color-badge/color-badge.component"
+import { ColorBadgeComponent } from "./color-badge/color-badge.component";
+import { FootbarComponent } from './footbar/footbar.component'
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
@@ -63,7 +64,8 @@ const appRoutes: Routes = [
     CollectionCreationComponent,
     CollectionItemsComponent,
     ItemCreationComponent,
-    ColorBadgeComponent
+    ColorBadgeComponent,
+    FootbarComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes, { enableTracing: true }),
