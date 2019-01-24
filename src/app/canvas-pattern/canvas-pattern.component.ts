16850c059c5069c0ccac4fd0f89251c31f18d690
vivien-mac <vivien@iMac-de-Nicolas-mac.local>
2019-01-24 13:55:36 +0100

    :sparkles: Display pattern in canvas

--- src/app/canvas-pattern/canvas-pattern.component.ts
+++ src/app/canvas-pattern/canvas-pattern.component.ts
@@ -16,22 +16,55 @@ import { Observable } from "rxjs"
 export class CanvasPatternComponent implements OnInit {
   @ViewChild("canvas") public canvas: ElementRef
   private _pattern: number[][]
+  private _imageWidth: number = 0
+  private _imageHeight: number = 0
+  static WIDTH = 400
+  static HEIGHT = 400
+  private cx: CanvasRenderingContext2D
 
   @Input()
   set pattern(pattern: number[][]) {
     this._pattern = pattern
+    this._imageWidth = this._pattern[0] ? this._pattern[0].length : 0
+    this._imageHeight = this._pattern.length
+    console.log(this._imageWidth, this._imageHeight)
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement
+    //
+    this.cx = canvasEl.getContext("2d")
+
+    canvasEl.width = CanvasPatternComponent.WIDTH
+    canvasEl.height = CanvasPatternComponent.HEIGHT
+
+    this.drawPattern(this._pattern)
   }
 
   constructor() {}
 
   ngOnInit() {}
 
-  ngAfterViewInit() {
+  ngAfterViewInit() {}
+
+  drawPattern(pattern: number[][]) {
     const canvasEl: HTMLCanvasElement = this.canvas.nativeElement
     //
     this.cx = canvasEl.getContext("2d")
-    //
-    console.log(canvasEl.width, canvasEl.height)
+    const elementSize = Math.round(
+      CanvasPatternComponent.HEIGHT /
+        (this._imageHeight > this._imageWidth
+          ? this._imageHeight
+          : this._imageWidth)
+    )
+    pattern.forEach((row, y) => {
+      row.forEach((el, x) => {
+        this.cx.fillText(
+          el,
+          x * elementSize,
+          y * elementSize + elementSize / 2,
+          elementSize,
+          elementSize
+        )
+      })
+    })
   }
 
   get pattern(): number[][] {
